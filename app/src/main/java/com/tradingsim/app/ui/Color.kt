9c90780b2df240cbfa94a7dc1c4c168f0f21aaef
Lucas <lucascapel@college-louezdieu.fr>
2026-07-28 14:37:07 +0200

    Add files via upload

---
+++ app/src/main/java/com/tradingsim/app/ui/Color.kt
@@ -0,0 +1,12 @@
+package com.tradingsim.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundDark = Color(0xFF0D1117)
+val CardDark = Color(0xFF161B22)
+val ProfitGreen = Color(0xFF00C853)
+val LossRed = Color(0xFFFF3D57)
+val AccentBlue = Color(0xFF4FC3F7)
+val TextWhite = Color(0xFFFFFFFF)
+val TextMuted = Color(0xFF8B949E)
+val BorderSubtle = Color(0xFF21262D)
